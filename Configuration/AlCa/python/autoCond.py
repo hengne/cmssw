f1e31e1b0a1042a5e23cdcfe744b931d1d8a81f6
Hengne <Hengne.Li@CERN.CH>
2016-04-14 14:22:34 +0200

    change gt to HBHENegEFilter reference GT, same insertion time as the HBHE negtive e filter recored, but without the negtive fiter record

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,7 +24,7 @@ autoCond = {
     # GlobalTag for Run2 data reprocessing
     'run2_data'         :   '80X_dataRun2_v8',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '80X_dataRun2_relval_HBHENegativeEFilter_Valid_v0',
+    'run2_data_relval'  :   '80X_dataRun2_relval_HBHENegativeEFilter_reference',
     # GlobalTag for Run1 HLT: it points to the online GT
     'run1_hlt'          :   '80X_dataRun2_HLT_frozen_v6',
     # GlobalTag for Run2 HLT: it points to the online GT
